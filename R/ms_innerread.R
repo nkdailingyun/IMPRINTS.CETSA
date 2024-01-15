747a4c2326b25943991ba3058bfee905d3aed3a1
mgerault <67689583+mgerault@users.noreply.github.com>
2024-01-15 15:27:00 +0100

    Add files via upload

--- R/ms_innerread.R
+++ R/ms_innerread.R
@@ -9,6 +9,8 @@
 #' @param fdrcontrol whether to check the protein FDR confidence level
 #' @param refchannel names of reference channel used in Proteome Discoverer search, such as 126
 #' @param channels names of the read-in channels
+#' @param software Software used to obtain Protein Group files. Either PD, MaxQuant or pFind.
+#'   Default is PD
 #'
 #' @keywords internal
 #'
@@ -18,7 +20,7 @@
 #'
 #'
 ms_innerread <- function(file, fchoose, treatment, nread,
-                         fdrcontrol, refchannel, channels) {
+                         fdrcontrol, refchannel, channels, software) {
 
   # Treatment and channel check
   if (length(treatment) != nread | length(channels) != nread) {
@@ -41,23 +43,47 @@ ms_innerread <- function(file, fchoose, treatment, nread,
   names(data) <- gsub(pattern="\\)", "", names(data))
 
   # create condition vector
-  conditions <- grep(pattern = "^Abundance: |^Abundances: ", colnames(data), value = TRUE)
-  if (length(conditions)==0) {
-    conditions <- grep(pattern = "^Abundances Grouped: ", colnames(data), value = TRUE)
+  if(software == "PD"){
+    conditions <- grep(pattern = "^Abundance: |^Abundances: ", colnames(data), value = TRUE)
+    if (length(conditions)==0) {
+      conditions <- grep(pattern = "^Abundances Grouped: ", colnames(data), value = TRUE)
+    }
+    conditions <- gsub(pattern = ".*, ", "", conditions)
+    conditions <- unique(conditions)
+    if (length(conditions) != 1) {
+      stop("Make sure the condition was correctedly specified. Specify one unique condition for each input file.")
+    }
   }
-  conditions <- gsub(pattern = ".*, ", "", conditions)
-  conditions <- unique(conditions)
-  if (length(conditions) != 1) {
-    stop("Make sure the condition was correctedly specified. Specify one unique condition for each input file.")
+  else if(software == "MaxQuant"){
+    conditions <- grep(pattern = "^Reporter intensity corrected \\d{1,}", colnames(data), value = TRUE)
+    if (length(conditions)==0) {
+      conditions <- grep(pattern = "^Reporter intensity \\d{1,}", colnames(data), value = TRUE)
+    }
+    conditions <- gsub(pattern = ".* ", "", conditions)
+    conditions <- unique(conditions)
+    if (length(conditions) != 1) {
+      stop("Make sure the condition was correctedly specified. Specify one unique condition for each input file.")
+    }
   }
   # print(conditions)
 
   # Remove low confidence proteins
   if (fdrcontrol) {
-    pattern <- grep("Protein FDR Confidence", names(data), value=FALSE)
-    if (length(pattern) > 0) {
-      names(data)[pattern] <- "FDR"
-      pattern <- grep("Low", data$FDR)
+    if(software == "PD"){
+      pattern <- grep("Protein FDR Confidence", names(data), value=FALSE)
+    }
+    else if(software == "MaxQuant"){
+      pattern <- grep("^Q-value$", names(data), value=FALSE)
+    }
+
+    if (length(pattern) == 1) {
+      if(software == "PD"){
+        pattern <- grep("Low", data[[pattern]])
+      }
+      else if(software == "MaxQuant"){
+        pattern <- which(data[[pattern]] <= 0)
+      }
+
       if (length(pattern) > 0) {
         message(paste0("There are ",length(pattern)," low confidence proteins in the origial data."))
         message("These low confidence proteins are removed from downstream analysis!")
@@ -88,15 +114,30 @@ ms_innerread <- function(file, fchoose, treatment, nread,
     colnames <- names(data)
   }
 
-  id <- grep(pattern = "^Accession$", colnames)
-  description <- grep(pattern = "^Description$", colnames)
-  sumUnipeps <-  grep(pattern = "^# Unique Peptides$", colnames)
-  sumPSMs <-  grep(pattern = "^# PSMs$", colnames)
-  countNum <- grep(pattern = paste0("^Abundances Count: [A-z0-9,. -]+",": ",
-                                    refchannel, "[A-z0-9,. -]+$"), colnames)
-  raw_abundance <- grep(pattern = "^Abundance: |^Abundances: ", colnames)
-  if (length(raw_abundance)==0) {
-    raw_abundance <- grep(pattern = "^Abundances Grouped: ", colnames)
+  if(software == "PD"){
+    id <- grep(pattern = "^Accession$", colnames)
+    description <- grep(pattern = "^Description$", colnames)
+    sumUnipeps <-  grep(pattern = "^# Unique Peptides$", colnames)
+    sumPSMs <-  grep(pattern = "^# PSMs$", colnames)
+    countNum <- grep(pattern = paste0("^Abundances Count: [A-z0-9,. -]+",": ",
+                                      refchannel, "[A-z0-9,. -]+$"), colnames)
+    raw_abundance <- grep(pattern = "^Abundance: |^Abundances: ", colnames)
+    if (length(raw_abundance)==0) {
+      raw_abundance <- grep(pattern = "^Abundances Grouped: ", colnames)
+    }
+  }
+  else if(software == "MaxQuant"){
+    id <- grep(pattern = "^Majority protein IDs$", colnames)
+    description <- grep(pattern = "^Fasta (H|h)eaders$", colnames)
+    sumUnipeps <-  grep(pattern = "^Unique (P|p)eptides$", colnames)
+    sumPSMs <-  grep(pattern = "^MS/MS (c|C)ount$", colnames)
+    countNum <- grep(pattern = paste0("^Reporter intensity count ",
+                                      which(channels == refchannel), " "), colnames)
+
+    raw_abundance <- grep(pattern = "^Reporter intensity corrected \\d{1,}", colnames(data))
+    if (length(raw_abundance)==0) {
+      raw_abundance <- grep(pattern = "^Reporter intensity \\d{1,}", colnames(data))
+    }
   }
 
   data <- data[ ,c(id, description, raw_abundance, sumUnipeps, sumPSMs, countNum)]
@@ -108,17 +149,43 @@ ms_innerread <- function(file, fchoose, treatment, nread,
   }
 
   data$condition <- conditions
-  colnames(data)[c((ncol(data)-3):(ncol(data)-1))] <- c("sumUniPeps", "sumPSMs", "countNum")  
+  colnames(data)[c((ncol(data)-3):(ncol(data)-1))] <- c("sumUniPeps", "sumPSMs", "countNum")
 
-  # be sure that channels are in same order as treatment vector
-  ord_channel <- sapply(channels,
-                        function(y) grep(paste0(" ", y, ","), colnames(data)))
-  data <- data[,c(1:2, ord_channel, (ncol(data)-3):ncol(data))]
+  if(software == "PD"){
+    # be sure that channels are in same order as treatment vector
+    ord_channel <- sapply(channels,
+                          function(y) grep(paste0(" ", y, ","), colnames(data)))
+    data <- data[,c(1:2, ord_channel, (ncol(data)-3):ncol(data))]
+  }
 
   # rename columns
   colnames(data) <- c("id", "description", treatment, "sumUniPeps",
                       "sumPSMs", "countNum", "condition")
   data <- data[,c(1:2, ncol(data), 3:(ncol(data)-1))]
+
+  if(software == "MaxQuant"){
+    # reformat description
+    data$description <- unlist(unname(sapply(data$description,
+                                             function(x){
+                                               x <- strsplit(x, ";")[[1]]
+                                               x <- unlist(lapply(strsplit(x, "\\|"),
+                                                                  function(y){
+                                                                    if(length(y) < 3){
+                                                                      y <- NULL
+                                                                    }
+                                                                    else{
+                                                                      y <- y[[3]]
+                                                                    };
+                                                                    y
+                                                                  })
+                                               )
+                                               x <- sub(".*? ", "", x)
+                                               x <- paste(x, collapse = ";");
+                                               x
+                                             }, simplify = FALSE)
+                                      )
+                               )
+  }
 
   return(data)
 }
